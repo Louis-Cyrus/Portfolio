3f02cbcf563aadba9f177010f6a9cbd616b1c285
Louis-Cyrus Sanjabi <87710724+Louis-Cyrus@users.noreply.github.com>
2023-04-21 15:47:13 +0200

    partie portfolio 75% done

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,21 +1,13 @@
 import Head from 'next/head';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import '@splidejs/react-splide/css';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import portfolioStyle from '../../src/pages/styles/Portfolio.module.scss';
-
-const options = {
-    perPage: 2,
-    arrows: true,
-    pagination: false,
-};
-
+import projectImages from './api/projectImages.json';
 
 export default function Portfolio() {
-    
     const [projects, setProjects] = useState([]);
 
     async function fetchRepositories() {
@@ -23,29 +15,30 @@ export default function Portfolio() {
             console.log('Fetching repositories...');
             const response = await axios.get('https://api.github.com/users/Louis-Cyrus/repos');
             console.log('Response from GitHub API:', response);
-            
+
             const repositories = response.data;
             console.log('Parsed repositories data:', repositories);
 
-            //Filtre des projets selon le langage
-            const languageFilter = [
-                "JavaScript",
-                "HTML",
-                "CSS",
-                "SCSS"
-            ];
+            // Filter projects based on language
+            const languageFilter = ["JavaScript", "HTML", "CSS", "SCSS"];
             const filteredProjects = repositories.filter((repository) =>
                 languageFilter.includes(repository.language)
             );
             console.log('Filtered repositories data:', filteredProjects);
-            
+
+            // Add image URLs to the filtered projects
+            filteredProjects.forEach((project) => {
+                project.imageUrl = projectImages[project.name] || "";
+            });
+            console.log('Filtered projects with image URLs:', filteredProjects);
+
             setProjects(filteredProjects);
             console.log('Repositories set in the state', projects);
-            
+
         } catch (error) {
             console.error('Error fetching repositories:', error);
         }
-    }  
+    }
 
     useEffect(() => {
         fetchRepositories();
@@ -61,35 +54,42 @@ export default function Portfolio() {
                 <div className={portfolioStyle.portfolioUnderline}></div>
 
                 <section className={portfolioStyle.portfolioProjects}>
-                    <div className="wrapper">
-                        <div className="splide">
-                            <Splide options={options} aria-label="My Favorite Images">
+                <Carousel
+                    showArrows={true}
+                    showStatus={false}
+                    showThumbs={false}
+                    infiniteLoop={true}
+                    centerMode={true}
+                    centerSlidePercentage={50}
+                >
+                    {projects.map((project) => (
+                        <div key={project.id} className={portfolioStyle.card}>
+
+                            <section className={portfolioStyle.cardImage}>
+                                <img src={project.imageUrl} alt={project.name} />
+                            </section>
+
+                            <section className={portfolioStyle.cardInfo}>
+                                <h3 className={portfolioStyle.projectTitle}>{project.name}</h3>
+                                <p className={portfolioStyle.projectDescription}>{project.description}</p>
                                 
-                                <SplideSlide>
-                                    {projects.map((project) => (
-                                        <div key={project.id} className={portfolioStyle.project}>
-                                            <h3 className={portfolioStyle.projectTitle}>{project.name}</h3>
-                                            <p className={portfolioStyle.projectDescription}>{project.description}</p>
-                                            <div className={portfolioStyle.projectLinks}>
-                                                <span className={portfolioStyle.projectLink}>
-                                                    <a href={project.html_url} target="_blank" rel="noopener noreferrer">
-                                                    View GitHub
-                                                    </a>
-                                                </span>
-                                                <span className={portfolioStyle.projectLink}>
-                                                    <a href={project.homepage} target="_blank" rel="noopener noreferrer">
-                                                        View Project
-                                                    </a>
-                                                </span>
-                                            </div>
-                                            <p>{project.language}</p>
-                                        </div>
-                                    ))}
-                                </SplideSlide>
-                                
-                            </Splide>
+                                <div className={portfolioStyle.projectLinks}>
+                                    <span className={portfolioStyle.projectLink}>
+                                        <a href={project.html_url} target="_blank" rel="noopener noreferrer" className={portfolioStyle.noUnderline}>
+                                            View GitHub
+                                        </a>
+                                    </span>
+                                    <span className={portfolioStyle.projectLink}>
+                                        <a href={project.homepage} target="_blank" rel="noopener noreferrer" className={portfolioStyle.noUnderline}>
+                                            View Project
+                                        </a>
+                                    </span>
+                                </div>
+                            </section>
+                            
                         </div>
-                    </div>
+                    ))}
+                </Carousel>
                 </section>
             </div>
         </>
